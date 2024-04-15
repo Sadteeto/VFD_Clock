e66923ddd9a912d8bb19aca21a7b05a3c184b28b
Your Name <you@example.com>
2024-04-16 02:13:23 +1000

    first commit

---
+++ src/pinsdef.h
@@ -0,0 +1,31 @@
+
+#define VFDOUT 2
+#define VFCLK 3
+#define VFLOAD 4
+#define VFBLANK 5
+#define GPSTX 6
+#define GPSRX 7
+
+
+// 7 segment display truth table
+#define segdispdot 0b00000001
+#define segdisp0 0b11111100
+#define segdisp1 0b01100000
+#define segdisp2 0b11011010
+#define segdisp3 0b11110010
+#define segdisp4 0b01100110
+#define segdisp5 0b10110110
+#define segdisp6 0b10111110
+#define segdisp7 0b11100000
+#define segdisp8 0b11111110
+#define segdisp9 0b11110110
+#define segdisp0dot 0b11111101
+#define segdisp1dot 0b01100001
+#define segdisp2dot 0b11011011
+#define segdisp3dot 0b11110011
+#define segdisp4dot 0b01100111
+#define segdisp5dot 0b10110111
+#define segdisp6dot 0b10111111
+#define segdisp7dot 0b11100001
+#define segdisp8dot 0b11111111
+#define segdisp9dot 0b11110111
