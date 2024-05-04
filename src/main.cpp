c2c92fbb348a39c51305a7665679b703d0506017
Teemo <you@example.com>
2024-05-05 00:40:13 +1000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,44 @@
 #include <Arduino.h>
 #include <pinsdef.h>
 #include <driver/spi_master.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 // // put function declarations here:
 
 
 
-// vfd data:
-// segmented display data
-// bit structure:
-// byte:   0 1 2 3 4 5 6 7 8 
-// digit : 1 2 3 4 5 6 7 8 9
-// bit:     0 1 2 3 4 5 6 7
-// segment: A B C D E F G Dot
+uint32_t segmentDigits[8];
+static uint32_t empty = 0b0;
+unsigned long counter = 0;
 
 
-static uint8_t g_vfdTable[21] = {segdispdot, segdisp0, segdisp1, segdisp2, segdisp3, segdisp4, segdisp5, segdisp6, segdisp7, segdisp8, segdisp9, segdisp0dot, segdisp1dot, segdisp2dot, segdisp3dot, segdisp4dot, segdisp5dot, segdisp6dot, segdisp7dot, segdisp8dot, segdisp9dot};
-volatile uint32_t g_vfdData[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-volatile long g_globalCounter = 0;
+static spi_transaction_t empty_screen = {
+    .length = 32,         // Number of bits to transmit
+    .tx_buffer = &empty,   // Data buffer
+    .rx_buffer = NULL     // No data to be received
+};
+
 
 spi_device_handle_t vfdspi;
 
-SemaphoreHandle_t xaccessVfdDataSemaphore = NULL;
-SemaphoreHandle_t xspiSemaphore = NULL;
+// SemaphoreHandle_t xaccessVfdDataSemaphore = NULL;
+// SemaphoreHandle_t xspiSemaphore = NULL;
 
-void vGpsTask(void *pvParameters);
-void vVfdUpdateTask(void *pvParameters);
-void displayDigit(uint8_t digit, uint8_t value);
-void spi_transaction_complete(spi_transaction_t *t);
+// void vGpsTask(void *pvParameters);
+// void vVfdUpdateTask(void *pvParameters);
+// void displayDigit(uint8_t digit, uint8_t value);
+// void spi_transaction_complete(spi_transaction_t *t);
+
+
+
+// function declarations
+uint32_t getSegmentShow(char c);
+void clearScreen(void);
+void processString(const char *input);
+
+
+
 
 
 void setup() {
@@ -34,9 +46,10 @@ void setup() {
   pinMode(VFLOAD, OUTPUT);
   pinMode(VFBLANK, OUTPUT);
   Serial.begin(115200);
+  Serial.println("Hello World");
 
-  xaccessVfdDataSemaphore = xSemaphoreCreateBinary();
-  xspiSemaphore = xSemaphoreCreateBinary();
+//   xaccessVfdDataSemaphore = xSemaphoreCreateBinary();
+//   xspiSemaphore = xSemaphoreCreateBinary();
 
 
   // setup spi bus
@@ -49,7 +62,6 @@ void setup() {
       .quadhd_io_num = -1,  // Not used
       .max_transfer_sz = 0  // Default transfer size
   };
-
   // Initialize the SPI bus
   ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO));
 
@@ -58,112 +70,107 @@ void setup() {
       .command_bits = 0,
       .address_bits = 0,
       .mode = 0,                    // SPI mode 0
-      .clock_speed_hz = APB_CLK_FREQ/80,  // Clock speed in Hz (10 kHz)
+      .clock_speed_hz = SPI_MASTER_FREQ_8M, //APB_CLK_FREQ/80,  // Clock speed in Hz (10 kHz)
       .spics_io_num = -1,           // CS pin (not used in this example)
       .queue_size = 7,              // Transactions queue size
-      .post_cb = spi_transaction_complete
   };
 
   // Add device to the bus
   ESP_ERROR_CHECK(spi_bus_add_device(SPI2_HOST, &devcfg, &vfdspi));
+  Serial.println("SPI configured");
 
-
-  xTaskCreatePinnedToCore(
-    vGpsTask, /* Function to implement the task */
-    "gpsTask", /* Name of the task */
-    2048,  /* Stack size in words */
-    NULL,  /* Task input parameter */
-    1,  /* Priority of the task */
-    NULL,  /* Task handle. */
-    0); /* Core where the task should run */
-  // vfd task
-  xTaskCreatePinnedToCore(
-    vVfdUpdateTask, /* Function to implement the task */
-    "vfdTask", /* Name of the task */
-    2048,  /* Stack size in words */
-    NULL,  /* Task input parameter */
-    1,  /* Priority of the task */
-    NULL,  /* Task handle. */
-    1); /* Core where the task should run */
-
-  xSemaphoreGive(xaccessVfdDataSemaphore);
+  clearScreen();
 }
 
-// hardware intettupt on SPI2 done
-void spi_transaction_complete(spi_transaction_t *t) {
-    // Give the semaphore to unblock the waiting task
-    // Serial.println("spi_transaction_complete, giving semaphore");
-    digitalWrite(VFLOAD, HIGH);
-    digitalWrite(VFLOAD, LOW);
-    xSemaphoreGive(xspiSemaphore);
-}
-
-
-
-void vVfdUpdateTask(void *pvParameters) {
-    uint32_t data = 0b11001100110011000000;
-
-    // data structure to send to Display Driver
-    spi_transaction_t t = {
-        .length = 20,         // Number of bits to transmit
-        .tx_buffer = &data,   // Data buffer
-        .rx_buffer = NULL     // No data to be received
-    };
-
-    vTaskDelay(500);
-    Serial.println("vfdUpdateTask started");
-    while (1) {
-      if( xSemaphoreTake(xaccessVfdDataSemaphore, portMAX_DELAY) == pdPASS){
-        // Serial.printf("vfdTask - globalCounter: %ld\n", g_globalCounter);
-        xSemaphoreGive(xaccessVfdDataSemaphore);
-
-        // take semaphore
-        // send data to display driver
-        // Serial.println("vfdTask - Transmitting data to display driver");
-        for (int i = 0; i < 9; i++) {
-            ESP_ERROR_CHECK(spi_device_transmit(vfdspi, &t));
-            // take semaphore
-            if( xSemaphoreTake(xspiSemaphore, portMAX_DELAY) == pdPASS){
-            }
-        }
-
-      }
-      // vTaskDelay(9 / portTICK_PERIOD_MS);
-    }
-}
-
-
-void vGpsTask(void *pvParameters) {
-    // Serial1.begin(9600, SERIAL_8N1, GPSRX, GPSTX, false, 1000, 10);
-    // enter critical section
-        Serial.println("vGpsTask started");
-    while (1) {
-      // take semaphore
-      // if( xSemaphoreTake(xaccessVfdDataSemaphore, portMAX_DELAY) == pdPASS){
-          Serial.printf("gpsTask - globalCounter: %ld\n", g_globalCounter);
-          // g_globalCounter--;
-      //     xSemaphoreGive(xaccessVfdDataSemaphore);
-      // }
-      vTaskDelay(10 / portTICK_PERIOD_MS);
-
-    }
-}
-
-void displayDigit(uint8_t digit, uint8_t value) {
-    // set digit
-    digitalWrite(VFDOUT, LOW);
-    shiftOut(VFCLK, VFLOAD, MSBFIRST, 1 << digit);
-    digitalWrite(VFLOAD, HIGH);
-    digitalWrite(VFLOAD, LOW);
-    // set value
-    digitalWrite(VFDOUT, HIGH);
-    shiftOut(VFCLK, VFLOAD, MSBFIRST, value);
-    digitalWrite(VFLOAD, HIGH);
-    digitalWrite(VFLOAD, LOW);
-}
 
 void loop() {
-  // nothing to do here
+    counter ++;
+    char buffer[9];  
+    sprintf(buffer, "%08ld", counter);
+
+    processString(buffer);
+
+    for (int i = 0; i < 8; i++) {
+        spi_transaction_t t = {
+            .length = 32,         // Number of bits to transmit
+            .tx_buffer = &segmentDigits[i],   // Data buffer
+            .rx_buffer = NULL     // No data to be received
+        };
+
+        if (spi_device_transmit(vfdspi, &t) != ESP_OK) {
+            Serial.println("Error");
+        }
+        digitalWrite(VFLOAD, HIGH);
+        digitalWrite(VFLOAD, LOW);
+    }
 }
 
 
+/**
+ * Returns the segment value to show on a display for a given character.
+ *
+ * This function takes a character as input and returns the corresponding segment value
+ * to display that character on a display. The character can be any alphanumeric character
+ * or a limited set of special characters. If the character is not recognized, the function
+ * returns the segment value for displaying nothing.
+ *
+ * @param c The character to display.
+ * @return The segment value to show on the display.
+ */
+uint32_t getSegmentShow(char c) {
+    switch (toupper(c)) {
+        case '0': return SEGMENT_SHOW_0;
+        case '1': return SEGMENT_SHOW_1;
+        case '2': return SEGMENT_SHOW_2;
+        case '3': return SEGMENT_SHOW_3;
+        case '4': return SEGMENT_SHOW_4;
+        case '5': return SEGMENT_SHOW_5;
+        case '6': return SEGMENT_SHOW_6;
+        case '7': return SEGMENT_SHOW_7;
+        case '8': return SEGMENT_SHOW_8;
+        case '9': return SEGMENT_SHOW_9;
+        case 'A': return SEGMENT_SHOW_A;
+        case 'C': return SEGMENT_SHOW_C;
+        case 'E': return SEGMENT_SHOW_E;
+        case 'F': return SEGMENT_SHOW_F;
+        case 'G': return SEGMENT_SHOW_G;
+        case 'H': return SEGMENT_SHOW_H;
+        case 'I': return SEGMENT_SHOW_I;
+        case 'J': return SEGMENT_SHOW_J;
+        case 'L': return SEGMENT_SHOW_L;
+        case 'O': return SEGMENT_SHOW_O;
+        case 'P': return SEGMENT_SHOW_P;
+        case 'S': return SEGMENT_SHOW_S;
+        case 'T': return SEGMENT_SHOW_T;
+        case 'U': return SEGMENT_SHOW_U;
+        case 'Z': return SEGMENT_SHOW_Z;
+        default:  return SEGMENT_SHOW_NONE;
+    }
+}
+
+/**
+ * Clears the screen of the VFD display.
+ * This function transmits the `empty_screen` data to the VFD display using SPI communication.
+ * If the transmission fails, it prints an error message to the serial monitor.
+ * Finally, it toggles the VFLOAD pin to update the display.
+ */
+void clearScreen(){
+    if (spi_device_transmit(vfdspi, &empty_screen) != ESP_OK){
+        Serial.println("Error");
+    }
+    digitalWrite(VFLOAD, HIGH);
+    digitalWrite(VFLOAD, LOW);
+}
+
+
+void processString(const char *input) {
+    if (strlen(input) != 8) {
+        printf("Error: String must be exactly 8 characters long.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < 8; i++) {
+        uint32_t digitMask = 1 << (31 - i); // Create digit mask starting from DIGIT_1 to DIGIT_8
+        segmentDigits[i] = getSegmentShow(input[i]) | digitMask;
+    }
+}
--- src/pinsdef.h
+++ src/pinsdef.h
@@ -1,3 +1,7 @@
+/**
+ * @file pinsdef.h
+ * @brief Defines pin mappings and segment display patterns for a VFD clock.
+ */
 
 #define VFDOUT 2
 #define VFCLK 3
@@ -7,25 +11,48 @@
 #define GPSRX 7
 
 
-// 7 segment display truth table
-#define segdispdot 0b00000001
-#define segdisp0 0b11111100
-#define segdisp1 0b01100000
-#define segdisp2 0b11011010
-#define segdisp3 0b11110010
-#define segdisp4 0b01100110
-#define segdisp5 0b10110110
-#define segdisp6 0b10111110
-#define segdisp7 0b11100000
-#define segdisp8 0b11111110
-#define segdisp9 0b11110110
-#define segdisp0dot 0b11111101
-#define segdisp1dot 0b01100001
-#define segdisp2dot 0b11011011
-#define segdisp3dot 0b11110011
-#define segdisp4dot 0b01100111
-#define segdisp5dot 0b10110111
-#define segdisp6dot 0b10111111
-#define segdisp7dot 0b11100001
-#define segdisp8dot 0b11111111
-#define segdisp9dot 0b11110111
+#define DIGIT_1 (1 << 31)
+#define DIGIT_2 (1 << 30)
+#define DIGIT_3 (1 << 29)
+#define DIGIT_4 (1 << 28)
+#define DIGIT_5 (1 << 27)
+#define DIGIT_6 (1 << 26)
+#define DIGIT_7 (1 << 25)
+#define DIGIT_8 (1 << 24)
+#define SEGMENT_G (1 << 23)
+#define SEGMENT_F (1 << 22)
+#define SEGMENT_E (1 << 21)
+#define SEGMENT_D (1 << 20)
+#define SEGMENT_C (1 << 19)
+#define SEGMENT_B (1 << 18)
+#define SEGMENT_A (1 << 17)
+#define DIGIT_0 (1 << 16)
+#define SEGMENT_DOT (1 << 8)
+
+#define SEGMENT_SHOW_0  (SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F)         // 0
+#define SEGMENT_SHOW_1  (SEGMENT_B | SEGMENT_C)                                                       // 1
+#define SEGMENT_SHOW_2  (SEGMENT_A | SEGMENT_B | SEGMENT_D | SEGMENT_E | SEGMENT_G)                    // 2
+#define SEGMENT_SHOW_3  (SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_G)                    // 3
+#define SEGMENT_SHOW_4  (SEGMENT_B | SEGMENT_C | SEGMENT_F | SEGMENT_G)                                // 4
+#define SEGMENT_SHOW_5  (SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_F | SEGMENT_G)                    // 5
+#define SEGMENT_SHOW_6  (SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G)        // 6
+#define SEGMENT_SHOW_7  (SEGMENT_A | SEGMENT_B | SEGMENT_C)                                            // 7
+#define SEGMENT_SHOW_8  (SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G) // 8
+#define SEGMENT_SHOW_9  (SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_F | SEGMENT_G)        // 9
+
+#define SEGMENT_SHOW_A  (SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_E | SEGMENT_F | SEGMENT_G)        // A
+#define SEGMENT_SHOW_C  (SEGMENT_A | SEGMENT_D | SEGMENT_E | SEGMENT_F)       
+#define SEGMENT_SHOW_E  (SEGMENT_A | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G)                    // E
+#define SEGMENT_SHOW_F  (SEGMENT_A | SEGMENT_E | SEGMENT_F | SEGMENT_G)                                // F
+#define SEGMENT_SHOW_G  (SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F)                    // G
+#define SEGMENT_SHOW_H  (SEGMENT_B | SEGMENT_C | SEGMENT_E | SEGMENT_F | SEGMENT_G)                    // H
+#define SEGMENT_SHOW_I  (SEGMENT_B | SEGMENT_C)                                                        // I
+#define SEGMENT_SHOW_J  (SEGMENT_B | SEGMENT_C | SEGMENT_D)                                            // J
+#define SEGMENT_SHOW_L  (SEGMENT_D | SEGMENT_E | SEGMENT_F)                                            // L
+#define SEGMENT_SHOW_O  (SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F)        // O
+#define SEGMENT_SHOW_P  (SEGMENT_A | SEGMENT_B | SEGMENT_E | SEGMENT_F | SEGMENT_G)                    // P
+#define SEGMENT_SHOW_S  (SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_F | SEGMENT_G)                    // S
+#define SEGMENT_SHOW_T  (SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G)                                // T
+#define SEGMENT_SHOW_U  (SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F)                    // U
+#define SEGMENT_SHOW_Z  (SEGMENT_A | SEGMENT_B | SEGMENT_D | SEGMENT_E | SEGMENT_G)                    // Z  
+#define SEGMENT_SHOW_NONE 0
